da102129063007e371f90bd53d316919fd1b57e8
linikerj <linikerenem@gmail.com>
2022-05-12 00:31:36 -0300

    initialize the project amcom-market-place

--- backend/settings.py
+++ backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@g1d3dgpqqdz7f2sct5&m=q#@aq@@hu-$f#h(&iux0i=$*)qyg'
+SECRET_KEY = 'django-insecure-iifhwpg$(pi(i74di1if+pbz6085_=rv)*bi$)-rt78#+xon$w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
