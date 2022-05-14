770191ed7a8199d8847efe978271b30f970ef956
Liniker Oliveira <linikerenem@gmail.com>
2022-05-14 16:57:30 -0300

    initialize the table

--- frontend/src/components/Table/index.jsx
+++ frontend/src/components/Table/index.jsx
@@ -23,6 +23,14 @@ export function Table() {
             />
             <Button color={"#049237"}>Adicionar</Button>
         </Flex>
+        <S.Table>
+            <S.Columns>
+            <S.Title widthTitle="25rem">Produto/Serviço</S.Title>
+            <S.Title widthTitle="15rem">Quantidade</S.Title>
+            <S.Title widthTitle="15rem">Preço unitário</S.Title>
+            <S.Title widthTitle="10rem">Total</S.Title>
+            </S.Columns>
+        </S.Table>
     </S.Container>
     </>
     )
