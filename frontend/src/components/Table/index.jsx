6c1107ae5ec9b7409357ce0e2babeafdcaa37600
Liniker Oliveira <linikerenem@gmail.com>
2022-05-14 16:39:24 -0300

    create archteture Table with pagtitle

--- frontend/src/components/Table/index.jsx
+++ frontend/src/components/Table/index.jsx
@@ -1,7 +1,20 @@
 import * as S from "./style";
 
 export function Table() {
+    return (
+    <>
     <S.Container>
-        <Table />
+        <S.TitlePag> Produtos </S.TitlePag>
     </S.Container>
+    <S.Container>
+        <S.TitlePag> Quantidade </S.TitlePag>
+    </S.Container>
+    <S.Container>
+        <S.TitlePag> Preço unitário </S.TitlePag>
+    </S.Container>
+    <S.Container>
+        <S.TitlePag> Total </S.TitlePag>
+    </S.Container>
+    </>
+    )
 }
--- frontend/src/components/Table/style.js
+++ frontend/src/components/Table/style.js
@@ -8,7 +8,22 @@ export const Table = styled.table`
 
 export const Columns = styled.tr``;
 
+export const Title = styled.th`
+  ${({ widthTitle }) => css`
+    padding-top: 12px;
+    padding-bottom: 12px;
+    text-align: left;
+    color: #000;
+    width: ${widthTitle};
+  `}
+`;
+
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
+`;
+
+export const TitlePag = styled.h2`
+  margin-bottom: 4rem;
+  font-weight: normal;
 `;
